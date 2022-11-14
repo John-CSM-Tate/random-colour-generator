7f5b6059f56d935088630ca9d4491ecf73b42c6c
John Tate <14020610+John-CSM-Tate@users.noreply.github.com>
2022-11-14 02:56:27 +0000

    Added Clipboard Feature

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,30 @@
+// Generates colour on load and on button click.
 document.onload = generatorColour();
-
 document.getElementById("gen-color").addEventListener("click", generatorColour);
-
 function generatorColour() {
-    // Create color
+    // Create color.
     let randColor = Math.floor(Math.random() * 16777215).toString(16)
-    // set text and background to color
+    // set text and background to color.
     document.getElementById("main-page").style.backgroundColor = "#" + randColor;
     document.getElementById("colour").innerHTML = "#" + randColor.toUpperCase();
+    document.getElementById("copy-text").value = "#" + randColor.toUpperCase();
+}
+
+// Copy colour HEX code to clipboard.
+document.getElementById("clipboard").addEventListener("click", copyClipboard);
+function copyClipboard() {
+    let colorCode = document.getElementById("copy-text");
+    colorCode.select();
+    colorCode.setSelectionRange(0, 7);
+    navigator.clipboard.writeText(colorCode.value)
+}
+
+// Tooltip Activation.
+document.querySelector(".tooltip").addEventListener("click", copytooltip);
+function copytooltip() {
+    if (this.classList.contain('active')) {
+        this.classList.remove('active');
+    } else {
+        this.classList.add('active');
+    }
 }
